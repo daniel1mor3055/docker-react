af882e302c7f1e546b0c6bbdfd07c8308e05fece
Daniel Mor <daniel.mor@appsflyer.com>
2023-04-12 23:51:00 +0300

    adding actions

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Kaki There!
+          Changes on feature branch
         </p>
         <a
           className="App-link"
